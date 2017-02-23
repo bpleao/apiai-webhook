f319697b29c26e35866ccdba4f07464314baa2c4
bpleao <bruno.leao@gmail.com>
2017-02-23 07:59:52 -0300

    adapted original example to Spirits Book chatbot

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
 from __future__ import print_function
@@ -15,9 +16,14 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+import random
+
 # Flask app should start in global layout
 app = Flask(__name__)
 
+responseDict = {
+("Definir","Deus"):["Que é Deus?"]
+}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -36,68 +42,38 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+	result = req.get("result")
+    if result.get("action") != "getSpiritsBookResponse":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
-    result = req.get("result")
+	
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
+    conceito = parameters.get("conceito")
+	
+    if conceito is None or len(conceito) == 0:
         return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+    intent = result.get("metadata").get("intentName")
+	
+	k = [intent]
+	k.extend(sorted(conceito))
+	t = tuple(k)
+	print(t)
+	
+	if t in responseDict.keys():
+		response = random.sample(responseDict[t],1)
+	else:
+		response = "Desculpe, não entendi. Por favor faça outra pergunta."
+	
     print("Response:")
-    print(speech)
+    print(response)
 
     return {
-        "speech": speech,
-        "displayText": speech,
+        # "speech": response,
+        "displayText": response,
         # "data": data,
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "apiai-webhook"
     }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
