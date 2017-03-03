3dc70d38918b813387bc99e6dd99bae74533b916
bpleao <bruno.leao@gmail.com>
2017-03-02 21:14:50 -0300

    debugging

--- app.py
+++ app.py
@@ -68,7 +68,8 @@ def processRequest(req):
     
     # TODO: parcial match of itens in key must also be considered. Is an order of priority also required?
     if t in responseDict.keys():
-        print("key found in dict")
+        print("key found in dict:")
+        print(responseDict[t])
         response = random.sample(responseDict[t],1)[0]
     else:
         response = "Desculpe, não entendi. Por favor faça outra pergunta."
